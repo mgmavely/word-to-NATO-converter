a000ea5195d18a73528cc1d353e3a4bcf868147d
mgmavely <mgmavely@gmail.com>
2021-02-02 20:58:50 -0500

    added error handling

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ with open("nato_phonetic_alphabet.csv") as data:
     for (index, row) in new_data_frame.iterrows():
         code_words = {row.letter: row.code for (index, row) in new_data_frame.iterrows()}
 
-inp = input("Enter a word! ")
-inp_to_code_words = [code_words[i.upper()] for i in inp]
-print(inp_to_code_words)
+
+
+while True:
+    inp = input("Enter a word! ")
+    try:
+        inp_to_code_words = [code_words[i.upper()] for i in inp]
+        print(inp_to_code_words)
+        break
+    except KeyError:
+        print("Sorry, only letters in the alphabet")
